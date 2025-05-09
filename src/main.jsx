195ac2b5c7d30b597da7e5e1315aa8ed31ef83c1
zayn-tech-info <zayntech.info@gmail.com>
2025-05-09 19:20:14 +0100

    Installed react router dom and created Homepage

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import HomePage from "./pages/HomePage.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <App>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </App>
+    </BrowserRouter>
+  </StrictMode>
+);

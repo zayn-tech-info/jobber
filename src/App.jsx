55607793c030e29a2b498c8e8191e54f45266096
zayn-tech-info <zayntech.info@gmail.com>
2025-05-10 18:25:50 +0100

    Created the last onboarding screen

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import SplashScreen from "./pages/SplashScreen";
 import { Route, Routes } from "react-router-dom";
 import OnboardingScreen from "./pages/onboardingscreens/OnboardingScreen";
 import OnboardingScreen2 from "./pages/onboardingscreens/onboardingscreen2";
+import OnboardingScreen3 from "./pages/onboardingscreens/onboardingscreen3";
+
 function App() {
   return (
     <>
@@ -11,6 +13,7 @@ function App() {
         <Route path="/" element={<SplashScreen />} />
         <Route path="/onboarding1" element={<OnboardingScreen />} />
         <Route path="/onboarding2" element={<OnboardingScreen2 />} />
+        <Route path="/onboarding3" element={<OnboardingScreen3 />} />
 
         <Route path="/home" element={<HomePage />} />
       </Routes>
---
+++ src/pages/onboardingscreens/onboardingscreen3.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { OnboardingScreen2 } from "../../utils";
+import { Link } from "react-router-dom";
+const onboardingscreen3 = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div>
+        <div className="pt-10 px-5 space-y-8">
+          <p className="text-xl text-gray-400">
+            <Link to="/home">Skip</Link>
+          </p>
+          <div>
+            <img src={OnboardingScreen2} />
+          </div>
+          <div className="text-2xl font-medium text-center mb-10 my-2">
+            <p>
+              Start applying and <br /> get a Job Now!
+            </p>
+          </div>
+
+          <div>
+            <Link to="/onboarding3">
+              <button className="bg-[#007AFF] text-white font-medium text-lg py-3 px-10 rounded-md w-full mt-3">
+                <p>Connect with Linkedin</p>
+              </button>
+            </Link>
+          </div>
+          <p className="text-center">OR</p>
+          <div className="flex gap-10 justify-center items-center">
+            <div>
+              <Link to="/onboarding1">
+                <button className="bg-gray-200 font-medium text-lg py-2 px-8 rounded-md w-full">
+                  <p>Back</p>
+                </button>
+              </Link>
+            </div>
+            <div>
+              <Link to="/onboarding1">
+                <button className="bg-[#FFBF46]  font-medium text-lg py-2 px-7 rounded-md w-full">
+                  <p>Sign In/Up</p>
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default onboardingscreen3;

324a12e82a732bc81cc302be393f692b9a526813
zayn-tech-info <zayntech.info@gmail.com>
2025-05-10 17:37:44 +0100

    Created the second onboarding screen

--- src/pages/onboardingscreens/onboardingscreen2.jsx
+++ src/pages/onboardingscreens/onboardingscreen2.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import { JobHuntImage } from "../../utils";
 
+import { Link } from "react-router-dom";
 const onboardingscreen2 = () => {
   return (
-    <div>onboardingscreen2</div>
-  )
-}
+    <div className="flex justify-center items-center h-screen">
+      <div>
+        <div className="pt-10 px-5 space-y-8">
+          <p className="text-xl text-gray-400">
+            <Link to="/home">Skip</Link>
+          </p>
+          <div>
+            <img src={JobHuntImage} />
+          </div>
+          <div className="text-2xl font-medium text-center mb-10 my-5">
+            <p>
+              Track all your job <br /> applicatons and don't get <br /> lost in the process{" "}
+            </p>
+          </div>
 
-export default onboardingscreen2
+          <div>
+            <Link to="/onboarding3">
+              <button className="bg-[#FFBF46] text-white font-medium text-lg py-3 px-10 rounded-md w-full mt-7">
+                <p>Next</p>
+              </button>
+            </Link>
+          </div>
+          <div>
+            <Link to="/onboarding1">
+              <button className="bg-gray-200 font-medium text-lg py-3 px-10 rounded-md w-full">
+                <p>Back</p>
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default onboardingscreen2;
--- src/utils/index.js
+++ src/utils/index.js
@@ -3,19 +3,25 @@ import StringImg2 from "../assets/images/welcomeScreeens/stringSpl-2.png";
 import OnboardingImg from "../assets/images/welcomeScreeens/Onboarding-cuate 1.png";
 import Hiring from "../assets/images/welcomeScreeens/Hiring-cuate 1.png";
 import Splash from "../assets/images/welcomeScreeens/splashImage.jpg";
+import jobhunt from "../assets/images/welcomeScreeens/Job-hunt-cuate.png";
+// Importing images for the splash screen and onboarding screens
 
+// Importing images for the job listing logos
 import LinkedIn from '../assets/images/jobListing/Linkedin.png'
 import AngelList from '../assets/images/jobListing/AngelList.png'
 import Grassdor from '../assets/images/jobListing/glassdor.png'
 import Monster from '../assets/images/jobListing/Monster.png'
 import Indeed from '../assets/images/jobListing/indeed.png'
 
+// Exporting the imported images for use in other components
 export const SplashImage = StringImg;
 export const SplashImage2 = StringImg2;
 export const OnboardingScreen1 = OnboardingImg;
 export const OnboardingScreen2 = Hiring;
 export const SplashScreenImage = Splash;
+export const JobHuntImage = jobhunt;
 
+// Exporting the job listing logos for use in other components
 export const LinkedInLogo = LinkedIn;
 export const AngelListLogo = AngelList;
 export const GrassdorLogo = Grassdor;

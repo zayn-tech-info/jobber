8f9769bd2e2415eed26177386f091d13bb7f7209
zayn-tech-info <zayntech.info@gmail.com>
2025-05-10 10:37:37 +0100

    Finished the splash screen

--- src/pages/SplashScreen.jsx
+++ src/pages/SplashScreen.jsx
@@ -1,15 +1,32 @@
 import React from "react";
 import { SplashImage, SplashImage2 } from "../utils";
 import style from "../moduleStyles/Splash.module.css";
+import { Link } from "react-router-dom";
 const SplashScreen = () => {
   return (
     <div className={style.splashScreen}>
       <div className="w-full h-full">
-        <div>
-          <p className="text-3xl font-bold">
-            The <span className="text-[#FFBF46] ">Best</span> Platform for
-            looking for <span className="text-white">Jobs</span>
+        <div className={style.splashImage}>
+          <div className="text-white flex flex-col p-3 justify-center items-center">
+            <span className="text-[#FFBF46]">Job</span>
+            <p>Searching</p>
+          </div>
+        </div>
+        <div className="mt-20 mx-auto flex justify-center items-center pt-10">
+          <p className="text-5xl font-bold">
+            The <span className="text-[#FFBF46] ">Best</span> <br />
+            Platform to <br />
+            looking for <br />
+            <span className="text-white">Jobs</span>
           </p>
+        </div>
+        <div className="mb-5 h-60">
+          <img src={SplashImage2} />
+        </div>
+        <div className="text-white font-medium text-center text-2xl">
+          <Link>
+            <p>Slide to Start {">>"}</p>
+          </Link>
         </div>
       </div>
     </div>
